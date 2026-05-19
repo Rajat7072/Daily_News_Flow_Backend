7e0fa3638527c6eda1690290f8adc66a8a1e86a6
Rajat7072 <109621985+Rajat7072@users.noreply.github.com>
2026-05-20 00:27:48 +0530

    adding console.log for origin

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     "https://dailynewsflow.com",
   ];
   const origin = req.headers.origin;
+  console.log("Origin is", origin);
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
