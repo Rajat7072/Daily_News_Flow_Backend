cdfc8e43c2f7350fc0e8bc8a816b03a56beae1b8
Rajat7072 <109621985+Rajat7072@users.noreply.github.com>
2026-05-19 23:12:39 +0530

    adding origins

--- server.js
+++ server.js
@@ -21,11 +21,27 @@ cloudinary.config({
 });
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [process.env.ALLOWED_URL, process.env.ALLOWED_URL_2],
-  }),
-);
+
+app.use((req, res, next) => {
+  const allowedOrigins = [process.env.ALLOWED_URL, process.env.ALLOWED_URL_2];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200); // respond to preflight
+    }
+    next();
+  } else {
+    return res.status(403).send("UnAuthorised Access");
+  }
+});
+
+// app.use(
+//   cors({
+//     origin: [process.env.ALLOWED_URL, process.env.ALLOWED_URL_2],
+//   }),
+// );
 
 app.use("/api", ExtractArticleRoute);
 app.use("/api", UploadImageRoute);
