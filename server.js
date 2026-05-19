4b482cbf9a96a97663fa6eee8a261e7cf16bced9
Rajat7072 <109621985+Rajat7072@users.noreply.github.com>
2026-05-20 00:14:48 +0530

    adding Headers

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     if (req.method === "OPTIONS") {
       return res.sendStatus(200); // respond to preflight
     }
