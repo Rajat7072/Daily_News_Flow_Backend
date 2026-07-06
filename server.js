061469a3d25b908b00b04ae5c2feb8a85a95e75c
Rajat7072 <109621985+Rajat7072@users.noreply.github.com>
2026-07-06 20:44:41 +0530

    adding server js changes

--- server.js
+++ server.js
@@ -37,9 +37,6 @@ app.use(
   }),
 );
 
-// Handle preflight requests
-app.options("*", cors());
-
 app.use("/newsapi", ExtractArticleRoute);
 app.use("/newsapi", UploadImageRoute);
 app.use("/newsapi", ArticleRoute);
